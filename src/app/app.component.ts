9506fd0be25a2118e7b6f1c5847fb9a0546b2037
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-25 13:46:30 +0200

    with the help of Gerard we could survive to this experience. Now the call to the api is done, but must do the disable button and mail correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FrontpageService} from "./frontpage.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  email = '';
+  submitted = false;
+
+  constructor(private frontpageService: FrontpageService){
+
+  }
+
+  subscribe(){
+    this.frontpageService.sendMail(this.email).subscribe(() => this.submitted = true, (e) => console.error(e));
+  }
 }
